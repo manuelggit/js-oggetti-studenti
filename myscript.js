bb16a1a3fa4b627f1debd613a21842ce73c47c98
Manuel Guerrini <man.guerrini@gmail.com>
2019-10-31 08:17:23 +0100

    Creo array di studenti

--- myscript.js
+++ myscript.js
@@ -15,3 +15,36 @@ console.log(studente);
 for (var proprietà in studente){
   console.log("le proprietà dello studente sono: " + proprietà + " e il loro valore è: " + studente[proprietà]);
 }
+
+var s1 = {
+  "nome": "Paolo",
+  "cognome": "Bianchi",
+  "età": "30",
+};
+
+var s2 = {
+  "nome": "Sergio",
+  "cognome": "Neri",
+  "età": "20",
+};
+
+var s3 = {
+  "nome": "Carlo",
+  "cognome": "Carli",
+  "età": "40",
+};
+
+var s4 = {
+  "nome": "Marta",
+  "cognome": "Nani",
+  "età": "32",
+};
+
+var s5 = {
+  "nome": "Alice",
+  "cognome": "Verdi",
+  "età": "25",
+};
+
+var aula = [s1, s2, s3, s4, s5];
+console.log(aula);
